2d9e84628d928d47ce2b7c666e8064b9d9813b52
menswear <MAZAKSIM@outlook.com>
2022-09-07 21:31:02 +0700

    SquareCalcTests

--- CalculationLibrary.Tests/SquareCalculationsTests.cs
+++ CalculationLibrary.Tests/SquareCalculationsTests.cs
@@ -1,15 +1,30 @@
 ﻿namespace CalculationLibrary.Tests;
 
-public class Tests
+public class SquareCalculationsTests
 {
-    [SetUp]
-    public void Setup()
+    [Test]
+    public void Radius3_expected28_2735()
     {
+        var r = 3;
+        var expected = 28.2735;
+
+        var squareCalculations = new SquareCalculations();
+        var actual = squareCalculations.CalcCircleSquareByR(r);
+
+        Assert.AreEqual(expected, actual);
     }
 
     [Test]
-    public void Test1()
+    public void SquareTriangleWithSides_3_4_5_expected_6()
     {
-        Assert.Pass();
+        var a = 3;
+        var b = 4;
+        var c = 5;
+        var expected = 6;
+
+        var squareCalculations = new SquareCalculations();
+        var actual = squareCalculations.CalcSquareTriangleBySides(a, b, c);
+
+        Assert.AreEqual(expected, actual);
     }
 }
